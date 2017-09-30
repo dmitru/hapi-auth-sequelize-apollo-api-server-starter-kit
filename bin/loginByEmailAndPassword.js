a5bc108af033a48f96d0feb4a8a8039b7d8a7f54
Dmitry Borody <dmitriy.borodiy@gmail.com>
2017-09-30 21:06:20 +0300

    Add a utility script for siging in as app users

---
+++ bin/loginByEmailAndPassword.js
@@ -0,0 +1,20 @@
+import { createSessionTokenAndLogin } from 'app/api/auth/utils';
+import models from 'app/models';
+
+(async () => {
+  try {
+    const email = process.argv[2];
+    const user = await models.User.findOne({ where: { email } });
+    if (!user) {
+      console.error('User not found:', email);
+      process.exit(1);
+      return;
+    }
+    const token = await createSessionTokenAndLogin(user);
+    console.log(token);
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(2);
+  }
+})();
