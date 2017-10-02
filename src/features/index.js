1498f89dc9e7d9f165a9f0d1215c87090c3045d2
Dmitry Borody <dmitriy.borodiy@gmail.com>
2017-10-02 16:55:43 +0300

    Implement auto-discovery of app features based on the directory layout

--- src/features/index.js
+++ src/features/index.js
@@ -1,7 +1,16 @@
-import users from './users';
-import graphqlTypes from './graphqlTypes';
+import glob from 'glob';
+import path from 'path';
+
+import logger from 'app/logger';
 
 import { combineFeatures } from './feature';
 
-const features = combineFeatures(users, graphqlTypes);
+const featureModules = [];
+glob.sync(path.join(__dirname, '*/index.js')).forEach((file) => {
+  logger.debug(`features: Adding feature ${file}`);
+  const featureModule = require(file).default;
+  featureModules.push(featureModule);
+});
+const features = combineFeatures(...featureModules);
+
 export default features;
